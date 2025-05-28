8045c106eb2b463b99b9c18ac5f67ea7783d9d48
SineWave <2293822350@qq.com>
2025-05-28 17:05:58 +0800

    添加ADC单次转换测试代码

--- User/UserTask.c
+++ User/UserTask.c
@@ -1,10 +1,17 @@
 #include "UserTask.h"
 
+uint16_t ADC0Data = 0;
+float ADC0Voltage = 0;
+
 void UserTask_init(void) {
-    
+    ADC_init();
 }
 
 void UserTask_loop(void) {
+    ADC0_start();
+    ADC0Data = ADC0_getData(10);
+    ADC0Voltage = ADC0_getVoltage(10);
+
     DL_GPIO_togglePins(GPIO_LED_PORT, GPIO_LED_LED1_PIN);
     Tick_delay(500);
 }
--- User/UserTask.h
+++ User/UserTask.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include "ti_msp_dl_config.h"
 #include "Tick.h"
+#include "ADC.h"
 
 void UserTask_init(void);
 void UserTask_loop(void);
